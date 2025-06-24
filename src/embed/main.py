27290dc95154790ff77bd67e71cf11ec1058be0d
Jamie Kemman <jamieruth@proton.me>
2025-06-24 11:00:46 +0300

    debugging

--- src/embed/main.py
+++ src/embed/main.py
@@ -254,6 +254,8 @@ class RepositoryEmbedder:
             i+=1
             self.collection_mapping.update({f"C{i}": (f"{key}", model_mapping.get("M3"))})
             i+=1
+        print(repos.keys())
+        print(collection_mapping.keys())
        # self.collection_mapping = {
        #     "A": ("java-microservice_primary", "jinaai/jina-embeddings-v2-base-code"),
        #     "B": ("java-microservice_primary", "nomic-ai/CodeRankEmbed"),
