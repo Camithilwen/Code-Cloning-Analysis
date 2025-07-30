f911a1945cd5520ed2d6d99321d61fac6577e68d
Shreya Nakum <shreyanakum2@gmail.com>
2025-07-30 10:10:35 +0300

    deleted codenet

--- src/llm-scripts/testing/Project_CodeNet_experimentation_dataset/data/p00542/s968223538.cpp
+++
@@ -1,9 +0,0 @@
-#include <algorithm>
-#include <iostream>
-using namespace std;
-
-int main() {
-	int A, B, C, D, E, F;
-	cin >> A >> B >> C >> D >> E >> F;
-	cout << max(E, F) + max({ A + B + C, A + B + D, A + C + D, B + C + D }) << endl;
-}
